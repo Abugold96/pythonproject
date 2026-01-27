da170975e48a08edcd0f13bc240426b5c7c6b8ab
Abugold96 <belloabubakardare96@gmail.com>
2026-01-27 13:55:30 +0100

    latest add  to the project

--- quizgame.py
+++ quizgame.py
@@ -2,11 +2,31 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay let's play: )")
 
 answer = input("What does CPU stands for?")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for?")
+if answer.lower() == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does HTML stand for?")
+if answer.lower() == "hypertext markup language":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does URL stand for?")
+if answer.lower() == "uniform resource locator":
+    print("Correct!")
+else:
+    print("Incorrect!")
