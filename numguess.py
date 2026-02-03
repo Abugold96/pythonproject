95a123527b34f1617e84e739fb1fb936e215e26c
Abugold96 <belloabubakardare96@gmail.com>
2026-02-03 12:30:19 +0100

    first commit in February

--- numguess.py
+++ numguess.py
@@ -9,7 +9,21 @@ if top_of_range.isdigit():
         print("Please type a number larger than 0 next time.. ")
         quit()
 else:
-    print("lease type a number next time... ")
+    print("Please type a number next time... ")
     quit()
 
-random_number = random.randint(top_of_range)
+random_number = random.randint(0, top_of_range)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong")
