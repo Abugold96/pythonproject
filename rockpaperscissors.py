68d2010f7d0d476eea01393574caa2b0bb797153
Abugold96 <belloabubakardare96@gmail.com>
2026-02-08 14:06:42 +0100

    Sunday commit

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,6 +4,7 @@ user_wins = 0
 computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
+
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
@@ -19,7 +20,21 @@ while True:
 
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
-        user_wins == 1
+        user_wins += 1
 
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
 
 print("Goodbye!")
