ed04d19b0c14ff7d1430ac4cd60848981c284470
Abugold96 <belloabubakardare96@gmail.com>
2026-03-31 18:06:42 +0100

    A new commit

--- passwordmanager/passwordmanager.py
+++ passwordmanager/passwordmanager.py
@@ -1,10 +1,10 @@
 from cryptography.fernet import Fernet
 
-'''
+
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)'''
+        key_file.write(key)
 
 
 def load_key():
@@ -25,14 +25,15 @@ def view():
             data = line.rstrip()
             user, passw = data.split("|")
             print("User:", user, "| Password:",
-                  fer.decrypt(passw.encode()))
+                  str(fer.decrypt(passw.encode())))
 
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
+
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+        f.write(name + "|" + str(fer.encrypt(pwd.encode()).decode() + "\n"))
 
 
 while True:
